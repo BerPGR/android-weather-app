6034981e6c0e6207fb6b12f4223dcf489bec93be
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-11-19 22:17:41 -0300

    Project iniated

---
+++ app/src/main/java/br/com/bernardo/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.bernardo.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //https://goweather.herokuapp.com/weather/
+    }
+}
